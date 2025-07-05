89c12e0f76d7961e92f18dc220a2d80e3bc3ba38
Emma Sysel <peter@sysel.at>
2025-07-05 17:38:07 +0200

    whoops much code no commits oje

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::time::Duration;
 
-use clap::{Parser, Subcommand};
+use clap::{Args as ClapArgs, Parser, Subcommand};
+use rand::Rng;
 
 const OPCODE_ACK: u8 = 0xBA;
 const OPCODE_NACK: u8 = 0xAA;
 const RX_TIMEOUT: Duration = Duration::from_millis(500);
+const MAX_TX_RETRIES: u8 = 3;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -16,28 +18,59 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Command {
     /// Calibrate the measured temperature using a reference value
+    ///
+    /// For testing purposes, a transmission error can be simulated using the '--fakeissue'
+    /// option. If no error probability is passed, a default value of 50% is used.
+    ///
+    /// Error simulation example:
+    ///
+    /// bikecmd tcalibrate --port COM3 --fakeissue=75 205
+    ///
+    /// The above command sends temperature calibration data for a current temperature of 20.5 C°,
+    /// with the probability of a simulated error occurring set to 75%.
+    ///
+    /// Note that the '--fakeissue' option has no effect on real errors occurring :)
+    ///
     #[command(name = "tcalibrate")]
     TemperatureCalibrate {
-        /// The serial port to use
-        #[arg(short = 'p', long = "port")]
-        serial_port: String,
-
-        /// The baud rate to use
-        #[arg(short = 'r', long = "rate", default_value = "115200")]
-        rate: u32,
-
-        /// The current temperature (in °C/10) to be used as reference
-        #[arg()]
-        current_temperature: i16,
-
-        /// Fakes a transmit error by sending an incorrect crc
-        #[arg(long)]
-        fakeissue: bool,
+        #[command(flatten)]
+        args: TemperatureCalibrateArgs,
     },
 
     /// List all avaliable serial ports
     #[command(name = "list")]
     ListDevices,
+}
+
+#[derive(ClapArgs, Clone, Debug)]
+struct SerialArgs {
+    /// The serial port to use
+    #[arg(short = 'p', long = "port")]
+    port: String,
+
+    /// The baud rate to use
+    #[arg(short = 'r', long = "rate", default_value = "115200")]
+    rate: u32,
+}
+
+#[derive(ClapArgs, Clone, Debug)]
+struct TemperatureCalibrateArgs {
+    /// The current temperature (in °C/10) to be used as reference
+    #[arg()]
+    current_temperature: i16,
+
+    /// Fakes a transmit error by sending an incorrect crc. The probability (0-100) can be specified.
+    #[arg(
+        long,
+        value_parser = clap::value_parser!(u8).range(0..=100),
+        default_missing_value="50",
+        value_name="PROBABILITY",
+        num_args = 0..=1,
+        require_equals = true)]
+    fakeissue: Option<u8>,
+
+    #[command(flatten)]
+    serial: SerialArgs,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -50,27 +83,26 @@ enum BikecmdError {
 
     #[error("Protocol Error: {0}")]
     BikecomputerProto(String),
+
+    #[error("Gave up after {0} retries")]
+    RetryStalled(u8),
 }
 
 fn main() {
     let args = Args::parse();
 
-    let result = match &args.command {
+    let result = match args.command {
         Command::ListDevices => list_devices(),
-        Command::TemperatureCalibrate {
-            serial_port,
-            rate,
-            current_temperature,
-            fakeissue,
-        } => temp_calibrate(serial_port, *rate, *current_temperature, *fakeissue),
+        Command::TemperatureCalibrate { args } => temp_calibrate(args),
     };
 
     match result {
         Ok(_) => (),
-        Err(e) => print!("An error occurred: {}", e),
+        Err(e) => println!("Exiting: {}", e),
     }
 }
 
+//list subcommand
 fn list_devices() -> Result<(), BikecmdError> {
     let ports = serialport::available_ports()?;
 
@@ -81,18 +113,39 @@ fn list_devices() -> Result<(), BikecmdError> {
     Ok(())
 }
 
-fn temp_calibrate(
-    port: &String,
-    rate: u32,
-    temp: i16,
-    fakeissue: bool,
-) -> Result<(), BikecmdError> {
-    let mut port = serialport::new(port, rate)
+//tcalibrate subcommand
+fn temp_calibrate(args: TemperatureCalibrateArgs) -> Result<(), BikecmdError> {
+    let mut retries = MAX_TX_RETRIES;
+    loop {
+        println!("Sending calibration data...");
+        match temp_calibrate_run(&args) {
+            Ok(_) => {
+                println!("Success");
+                return Ok(());
+            }
+            Err(e) => match &e {
+                //do not retry on permission problems
+                BikecmdError::Serial(inner) if inner.kind == serialport::ErrorKind::NoDevice => return Err(e),
+                e => {
+                    retries -= 1;
+                    println!("An error occurred: {}", e);
+                    if retries == 0 {
+                        return Err(BikecmdError::RetryStalled(MAX_TX_RETRIES));
+                    }
+                    println!("Retrying {} more time(s)\n", retries);
+                }
+            },
+        }
+    }
+}
+
+fn temp_calibrate_run(args: &TemperatureCalibrateArgs) -> Result<(), BikecmdError> {
+    let mut port = serialport::new(&args.serial.port, args.serial.rate)
         .timeout(RX_TIMEOUT)
         .open()?;
 
     // temperature as bytes (little endian)
-    let mut calibration_bytes = temp.to_le_bytes().to_vec();
+    let mut calibration_bytes = args.current_temperature.to_le_bytes().to_vec();
 
     //frame start: 0xFF frame start indicator, 0x02 payload length
     let mut message: Vec<u8> = vec![0xFF, 0x02];
@@ -101,10 +154,22 @@ fn temp_calibrate(
     //checksum only includes length and payload
     //the impl on the microcontroller uses the "XMODEM" variant of crc16
     let mut checksum = crc16::State::<crc16::XMODEM>::calculate(&message[1..4]);
+
+    //decide if we want an error
+    let fakeissue = match args.fakeissue {
+        None => false,
+        Some(probability) => {
+            let mut rng = rand::rng();
+            let random_value = rng.random::<u8>() % 100;
+            random_value < probability
+        }
+    };
+
     if fakeissue {
         //change checksum if we want to see an error
         checksum += 10;
     }
+
     //checksum bytes (also little endian)
     let mut checksum = checksum.to_le_bytes().to_vec();
     message.append(&mut checksum);
@@ -127,9 +192,7 @@ fn temp_calibrate(
     match received[0] {
         OPCODE_ACK => Ok(()),
         //TODO retry sending another time on NACK
-        OPCODE_NACK => Err(BikecmdError::BikecomputerProto(
-            "Received NACK, please retry".to_string(),
-        )),
+        OPCODE_NACK => Err(BikecmdError::BikecomputerProto("Received NACK".to_string())),
         //catch-all
         symbol => Err(BikecmdError::BikecomputerProto(format!(
             "ACK not received, received {:#04x} instead",
